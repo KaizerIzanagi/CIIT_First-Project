b5e7d41617d8768036f5e165737ea5e7b3449af0
KaizerIzanagi <134472157+KaizerIzanagi@users.noreply.github.com>
2023-05-31 14:58:04 +0800

    I made stuff today
    "I am in awe of what I have made today, yet I feel like I have made too little."
    
    - Theseus, "The Persian War of 300"

---
+++ Assets/Scripts/TextGameManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextGameManager : MonoBehaviour
+{
+    // Look man I just want to make a text based horror game about Dead Hard
+    public float healthValue, willpowerValue;
+    // Oh my god this will kill my PC's storage space
+    public TextMeshProUGUI hpTextValue, willpowerTextValue, storyTextValue;
+    // Now, DO IT MY DOOD! WRITE!!!
+    public string storyText;
+    // Game Object to make "Proofs" go "Poof"!
+    public GameObject level1_Choices;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthValue = 100;
+        willpowerValue = 100;
+        storyText = "Ugh... where am I?";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hpTextValue.text = healthValue.ToString();
+        willpowerTextValue.text = willpowerValue.ToString();
+        storyTextValue.text = storyText.ToString();
+    }
+
+    public void ScreamForHelp()
+    {
+        // "HP -= x" is the same as "HP = HP - x", but shorter
+        healthValue -= 33;
+        willpowerValue -= 33;
+        storyText = "Your Scream attracted the killer to your location, and now the chase is on! You see a Pallet, a Window, and an Open Field, where do you run to?";
+    }
+
+    public void LookAround()
+    {
+        storyText = "You look around and see a Generator, a Chest, and an Open Field. What do you do?";
+    }
+    public void Panic()
+    {
+        healthValue -= 100;
+        willpowerValue -= 100;
+        storyText = "Your Panic attracted the killer to your location, and you have been sacrificed immediately! Try again?";
+        level1_Choices.SetActive(false);
+    }
+
+
+
+
+
+}
